1675fe6494868e4310426ea20b355d6340d91238
Szabolcs Andrasi <andrasi.szabolcs@gmail.com>
2012-02-08 08:24:52 +0100

    Add version support to Character entity

---
+++ movie-catalog-domain/src/test/scala/com/github/sandrasi/moviecatalog/domain/entities/base/VersionSupportTest.scala
@@ -0,0 +1,13 @@
+package com.github.sandrasi.moviecatalog.domain.entities.base
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: sandrasi
+ * Date: 2/7/12
+ * Time: 10:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class VersionSupportTest {
+
+}
