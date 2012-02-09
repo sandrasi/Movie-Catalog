fe17e59b4bc2bafc331ec7c205709f980383093c
Szabolcs Andrasi <andrasi.szabolcs@gmail.com>
2012-02-09 20:24:30 +0100

    Delete old files

--- movie-catalog-domain/src/main/scala/com/github/sandrasi/moviecatalog/domain/entities/base/LongIdEntity.scala
+++
@@ -1,8 +0,0 @@
-package com.github.sandrasi.moviecatalog.domain.entities.base
-
-import com.github.sandrasi.moviecatalog.common.Validate
-
-abstract class LongIdEntity(id: Long) extends BaseEntity[Long](if (id != 0) Some(id) else None) {
-
-  Validate.isTrue(id >= 0)
-}
