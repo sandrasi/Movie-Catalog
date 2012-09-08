41a2837f36d1e7bc9291da8d4d22a87e2a0dcef6
Szabolcs Andrasi <andrasi.szabolcs@gmail.com>
2012-09-08 12:28:36 +0200

    Implement adding unique digital container

--- movie-catalog-repository-neo4j/src/main/scala/com/github/sandrasi/moviecatalog/repository/neo4j/utility/UniqueNodeFactory.scala
+++ movie-catalog-repository-neo4j/src/main/scala/com/github/sandrasi/moviecatalog/repository/neo4j/utility/UniqueNodeFactory.scala
@@ -9,6 +9,7 @@ import org.neo4j.index.lucene.ValueContext
 import com.github.sandrasi.moviecatalog.common.Validate
 import com.github.sandrasi.moviecatalog.domain.entities.base.VersionedLongIdEntity
 import com.github.sandrasi.moviecatalog.domain.entities.castandcrew.AbstractCast
+import com.github.sandrasi.moviecatalog.domain.entities.container.{Soundtrack, DigitalContainer}
 import com.github.sandrasi.moviecatalog.domain.entities.core.Character
 import com.github.sandrasi.moviecatalog.repository.neo4j.relationshiptypes.CharacterRelationshipType._
 import com.github.sandrasi.moviecatalog.repository.neo4j.relationshiptypes.FilmCrewRelationshipType
@@ -16,6 +17,7 @@ import com.github.sandrasi.moviecatalog.repository.neo4j.utility.MovieCatalogDbC
 import com.github.sandrasi.moviecatalog.repository.neo4j.utility.PropertyManager._
 import org.neo4j.graphdb._
 import org.neo4j.graphdb.Direction._
+import com.github.sandrasi.moviecatalog.repository.neo4j.relationshiptypes.DigitalContainerRelationshipType._
 
 private[utility] class UniqueNodeFactory(db: GraphDatabaseService) {
 
@@ -26,10 +28,12 @@ private[utility] class UniqueNodeFactory(db: GraphDatabaseService) {
   private final val IdxMgr = db.index()
   private final val CastIndex = IdxMgr.forRelationships("Cast")
   private final val CharacterIndex = IdxMgr.forNodes("Characters")
+  private final val DigitalContainerIndex = IdxMgr.forRelationships("DigitalConainers")
   
   def createNodeFrom(e: VersionedLongIdEntity)(implicit tx: Transaction): Node = e match {
     case ac: AbstractCast => lock(ac) { setNodePropertiesFrom(createNode(ac), ac) }
     case c: Character => lock(c) { setNodePropertiesFrom(createNode(c), c) }
+    case dc: DigitalContainer => lock(dc) { setNodePropertiesFrom(createNode(dc), dc) }
     case _ => throw new IllegalArgumentException("Unsupported entity type: %s".format(e.getClass.getName))
   }
 
@@ -62,6 +66,18 @@ private[utility] class UniqueNodeFactory(db: GraphDatabaseService) {
     n
   }
 
+  private def setNodePropertiesFrom(n: Node, dc: DigitalContainer): Node = withExistenceCheck(dc) {
+    n.getRelationships(WithContent, OUTGOING).asScala.foreach(_.delete())
+    n.getRelationships(WithSoundtrack, OUTGOING).asScala.foreach(_.delete())
+    n.getRelationships(WithSubtitle, OUTGOING).asScala.foreach(_.delete())
+    n.createRelationshipTo(getNode(dc.motionPicture), WithContent)
+    dc.soundtracks.map(getNode(_)).foreach(n.createRelationshipTo(_, WithSoundtrack))
+    dc.subtitles.map(getNode(_)).foreach(n.createRelationshipTo(_, WithSubtitle))
+    setVersion(n, dc)
+    index(n, dc)
+    n
+  }
+
   private def setVersion(n: Node, e: VersionedLongIdEntity) {
     if (e.id != None && !hasExpectedVersion(n, e.version)) throw new IllegalStateException("%s is out of date".format(e))
     setLong(n, Version, if (e.id == None) e.version else e.version + 1)
@@ -71,11 +87,11 @@ private[utility] class UniqueNodeFactory(db: GraphDatabaseService) {
   
   private def index(n: Node, ac: AbstractCast) {
     CastIndex.get("type", FilmCrewRelationshipType.forClass(ac.getClass), n, null).iterator.asScala.foreach(CastIndex.remove(_))
-    CastIndex.get("type", Played.name, n, null).iterator.asScala.foreach(CastIndex.remove(_))
-    CastIndex.get("type", AppearedIn.name, n, null).iterator.asScala.foreach(CastIndex.remove(_))
+    CastIndex.get("played", Played.name, n, null).iterator.asScala.foreach(CastIndex.remove(_))
+    CastIndex.get("appearedIn", AppearedIn.name, n, null).iterator.asScala.foreach(CastIndex.remove(_))
     CastIndex.add(n.getSingleRelationship(FilmCrewRelationshipType.forClass(ac.getClass), OUTGOING), "type", FilmCrewRelationshipType.forClass(ac.getClass).name)
-    CastIndex.add(n.getSingleRelationship(Played, OUTGOING), "type", Played.name)
-    CastIndex.add(n.getSingleRelationship(AppearedIn, OUTGOING), "type", AppearedIn.name)
+    CastIndex.add(n.getSingleRelationship(Played, OUTGOING), "played", Played.name)
+    CastIndex.add(n.getSingleRelationship(AppearedIn, OUTGOING), "appearedIn", AppearedIn.name)
   }
 
   private def index(n: Node, c: Character) {
@@ -84,18 +100,28 @@ private[utility] class UniqueNodeFactory(db: GraphDatabaseService) {
     CharacterIndex.add(n, CharacterDiscriminator, c.discriminator)
   }
 
+  private def index(n: Node, dc: DigitalContainer) {
+    DigitalContainerIndex.get("withContent", WithContent.name, n, null).iterator().asScala.foreach(DigitalContainerIndex.remove(_))
+    DigitalContainerIndex.get("withSoundtrack", WithSoundtrack.name, n, null).iterator().asScala.foreach(DigitalContainerIndex.remove(_))
+    DigitalContainerIndex.get("withSubtitle", WithSubtitle.name, n, null).iterator().asScala.foreach(DigitalContainerIndex.remove(_))
+    DigitalContainerIndex.add(n.getSingleRelationship(WithContent, OUTGOING), "withContent", WithContent.name)
+    n.getRelationships(WithSoundtrack, OUTGOING).iterator().asScala.foreach(DigitalContainerIndex.add(_, "withSoundtrack", WithSoundtrack.name))
+    n.getRelationships(WithSubtitle, OUTGOING).iterator().asScala.foreach(DigitalContainerIndex.add(_, "withSubtitle", WithSubtitle.name))
+  }
+
   private def withExistenceCheck(e: VersionedLongIdEntity)(dbOp: => Node) = if (!exists(e)) dbOp else throw new IllegalArgumentException("Entity %s already exists in the repository".format(e))
 
   private def exists(e: VersionedLongIdEntity): Boolean = e match {
     case ac: AbstractCast => exists(ac)
     case c: Character => exists(c)
+    case dc: DigitalContainer => exists(dc)
     case _ => throw new IllegalArgumentException("Unsupported entity type: %s".format(e.getClass.getName))
   }
-  
+
   private def exists(ac: AbstractCast): Boolean = {
     val castsWithSamePerson = CastIndex.get("type", FilmCrewRelationshipType.forClass(ac.getClass).name, null, getNode(ac.person)).iterator.asScala.map(_.getStartNode).toSet
-    val castsWithSameCharacter = CastIndex.get("type", Played.name, null, getNode(ac.character)).iterator.asScala.map(_.getStartNode).toSet
-    val castsWithSameMotionPicture = CastIndex.get("type", AppearedIn.name, null, getNode(ac.motionPicture)).iterator.asScala.map(_.getStartNode).toSet
+    val castsWithSameCharacter = CastIndex.get("played", Played.name, null, getNode(ac.character)).iterator.asScala.map(_.getStartNode).toSet
+    val castsWithSameMotionPicture = CastIndex.get("appearedIn", AppearedIn.name, null, getNode(ac.motionPicture)).iterator.asScala.map(_.getStartNode).toSet
     (castsWithSamePerson & castsWithSameCharacter & castsWithSameMotionPicture).size > 0
   }
 
@@ -103,12 +129,19 @@ private[utility] class UniqueNodeFactory(db: GraphDatabaseService) {
     val query = new BooleanQuery()
     query.add(new TermQuery(new Term(CharacterName, c.name)), MUST)
     query.add(new TermQuery(new Term(CharacterDiscriminator, c.discriminator)), MUST)
-    val indexHits = CharacterIndex.query(query)
-    indexHits.getSingle != null
+    CharacterIndex.query(query).getSingle != null
+  }
+
+  private def exists(dc: DigitalContainer): Boolean = {
+    val dcsWithSameMotionPicture = DigitalContainerIndex.get("withContent", WithContent.name, null, getNode(dc.motionPicture)).iterator().asScala.map(_.getStartNode).toSet
+    val dcSetWithSameSoundtracks = for (s <- dc.soundtracks) yield DigitalContainerIndex.get("withSoundtrack", WithSoundtrack.name, null, getNode(s)).iterator().asScala.map(_.getStartNode).toSet
+    val dcSetWithSameSubtitles = for (s <- dc.subtitles) yield DigitalContainerIndex.get("withSubtitle", WithSubtitle.name, null, getNode(s)).iterator().asScala.map(_.getStartNode).toSet
+    val dcsWithSameSoundtracks = if (dcSetWithSameSoundtracks.isEmpty) Set.empty[Node] else dcSetWithSameSoundtracks.reduce(_ & _)
+    val dcsWithSameSubtitles = if (dcSetWithSameSubtitles.isEmpty) Set.empty[Node] else dcSetWithSameSubtitles.reduce(_ & _)
+    (dcsWithSameMotionPicture & dcsWithSameSoundtracks & dcsWithSameSubtitles).filter((n: Node) => n.getRelationships(WithSoundtrack, OUTGOING).iterator().asScala.size == dc.soundtracks.size && n.getRelationships(WithSubtitle, OUTGOING).iterator().asScala.size == dc.subtitles.size).size > 0
   }
 
     //case l: Long => NumericRangeQuery.newLongRange(k, l, l, true, true)
-  // TODO (sandrasi): acquire a read lock on the node
   private def getNode(e: VersionedLongIdEntity) = try {
     val node = if (e.id != None) db.getNodeById(e.id.get) else throw new IllegalStateException("%s is not in the database".format(e))
     if (SubrefNodeSupp.isNodeOfType(node, e.getClass)) node else throw new ClassCastException("Node [id: %d] is not of type %s".format(e.id.get, e.getClass.getName))
--- movie-catalog-repository-neo4j/src/test/scala/com/github/sandrasi/moviecatalog/repository/neo4j/utility/UniqueNodeFactoryTest.scala
+++ movie-catalog-repository-neo4j/src/test/scala/com/github/sandrasi/moviecatalog/repository/neo4j/utility/UniqueNodeFactoryTest.scala
@@ -7,9 +7,10 @@ import com.github.sandrasi.moviecatalog.domain.entities.castandcrew.Actor
 import com.github.sandrasi.moviecatalog.domain.entities.core.{Movie, Character, Person}
 import com.github.sandrasi.moviecatalog.domain.utility.Gender._
 import com.github.sandrasi.moviecatalog.repository.neo4j.test.utility.MovieCatalogNeo4jSupport
+import java.util.UUID
+import com.github.sandrasi.moviecatalog.domain.entities.container.DigitalContainer
 
 class UniqueNodeFactoryTest extends FunSuite with BeforeAndAfterAll with BeforeAndAfterEach with ShouldMatchers with MovieCatalogNeo4jSupport {
-
 
   private var subject: UniqueNodeFactory = _
 
@@ -17,7 +18,7 @@ class UniqueNodeFactoryTest extends FunSuite with BeforeAndAfterAll with BeforeA
     subject = UniqueNodeFactory(db)
   }
   
-  test("should not create node from actor it a node already exists") {
+  test("should not create node from actor if a node already exists") {
     val actor = Actor(insertEntity(JohnDoe), insertEntity(Johnny), insertEntity(TestMovie))
     implicit val tx = db.beginTx()
     transaction(tx) {
@@ -33,8 +34,10 @@ class UniqueNodeFactoryTest extends FunSuite with BeforeAndAfterAll with BeforeA
   }
   
   test("should create node from actor if a different person played the same character in the same movie") {
-    val actor = Actor(insertEntity(JohnDoe), insertEntity(Johnny), insertEntity(TestMovie))
-    val anotherActor = Actor(insertEntity(Person("James Doe", Male, new LocalDate(1970, 7, 7), "Anytown")), insertEntity(Johnny), insertEntity(TestMovie))
+    val character = insertEntity(Johnny)
+    val movie = insertEntity(TestMovie)
+    val actor = Actor(insertEntity(JohnDoe), character, movie)
+    val anotherActor = Actor(insertEntity(Person("James Doe", Male, new LocalDate(1970, 7, 7), "Anytown")), character, movie)
     implicit val tx = db.beginTx()
     transaction(tx) {
       val actorNode = subject.createNodeFrom(actor)
@@ -44,8 +47,10 @@ class UniqueNodeFactoryTest extends FunSuite with BeforeAndAfterAll with BeforeA
   }
 
   test("should create node from actor if a the same person played a different character in the same movie") {
-    val actor = Actor(insertEntity(JohnDoe), insertEntity(Johnny), insertEntity(TestMovie))
-    val anotherActor = Actor(insertEntity(JohnDoe), insertEntity(Character("Jamie")), insertEntity(TestMovie))
+    val person = insertEntity(JohnDoe)
+    val movie = insertEntity(TestMovie)
+    val actor = Actor(person, insertEntity(Johnny), movie)
+    val anotherActor = Actor(person, insertEntity(Character("Jamie")), movie)
     implicit val tx = db.beginTx()
     transaction(tx) {
       val actorNode = subject.createNodeFrom(actor)
@@ -55,8 +60,10 @@ class UniqueNodeFactoryTest extends FunSuite with BeforeAndAfterAll with BeforeA
   }
 
   test("should create node from actor if a the same person played the same character in a different movie") {
-    val actor = Actor(insertEntity(JohnDoe), insertEntity(Johnny), insertEntity(TestMovie))
-    val anotherActor = Actor(insertEntity(JohnDoe), insertEntity(Johnny), insertEntity(Movie("Foo movie title")))
+    val person = insertEntity(JohnDoe)
+    val character = insertEntity(Johnny)
+    val actor = Actor(person, character, insertEntity(TestMovie))
+    val anotherActor = Actor(person, character, insertEntity(Movie("Foo movie title")))
     implicit val tx = db.beginTx()
     transaction(tx) {
       val actorNode = subject.createNodeFrom(actor)
@@ -76,6 +83,110 @@ class UniqueNodeFactoryTest extends FunSuite with BeforeAndAfterAll with BeforeA
       transaction(tx) {
         subject.createNodeFrom(Johnny)
       }
+    }
+  }
+
+  test("should create node from character if the name is different") {
+    val anotherCharacter = Character("Jenny", Johnny.discriminator)
+    implicit val tx = db.beginTx()
+    transaction(tx) {
+      val characterNode = subject.createNodeFrom(Johnny)
+      val anotherCharacterNode = subject.createNodeFrom(anotherCharacter)
+      characterNode.getId should not equal(anotherCharacterNode.getId)
+    }
+  }
+
+  test("should create node from character if the discriminator is different") {
+    val anotherCharacter = Character(Johnny.name, UUID.randomUUID.toString)
+    implicit val tx = db.beginTx()
+    transaction(tx) {
+      val characterNode = subject.createNodeFrom(Johnny)
+      val anotherCharacterNode = subject.createNodeFrom(anotherCharacter)
+      characterNode.getId should not equal(anotherCharacterNode.getId)
+    }
+  }
+
+  test("should not create node from digital container if a node already exists") {
+    val digitalContainer = DigitalContainer(insertEntity(TestMovie), Set(insertEntity(EnglishSoundtrack), insertEntity(HungarianSoundtrack)), Set(insertEntity(EnglishSubtitle), insertEntity(HungarianSubtitle)))
+    implicit val tx = db.beginTx()
+    transaction(tx) {
+      subject.createNodeFrom(digitalContainer)
+    }
+
+    intercept[IllegalArgumentException] {
+      implicit val tx = db.beginTx()
+      transaction(tx) {
+        subject.createNodeFrom(digitalContainer)
+      }
+    }
+  }
+
+  test("should create node from digital container if it contains a different motion picture with the same soundtracks and subtitles") {
+    val soundtracks = Set(insertEntity(EnglishSoundtrack), insertEntity(HungarianSoundtrack))
+    val subtitles = Set(insertEntity(EnglishSubtitle), insertEntity(HungarianSubtitle))
+    val digitalContainer = DigitalContainer(insertEntity(TestMovie), soundtracks, subtitles)
+    val anotherDigitalContainer = DigitalContainer(insertEntity(Movie("Foo movie title")), soundtracks, subtitles)
+    implicit val tx = db.beginTx()
+    transaction(tx) {
+      val digitalContainerNode = subject.createNodeFrom(digitalContainer)
+      val anotherDigitalContainerNode = subject.createNodeFrom(anotherDigitalContainer)
+      digitalContainerNode.getId should not equal(anotherDigitalContainerNode.getId)
+    }
+  }
+
+  test("should create node from digital container if it contains the same movie with different soundtracks and same subtitles") {
+    val movie = insertEntity(TestMovie)
+    val subtitles = Set(insertEntity(EnglishSubtitle), insertEntity(HungarianSubtitle))
+    val soundtrack = insertEntity(EnglishSoundtrack)
+    val digitalContainer = DigitalContainer(movie, Set(soundtrack, insertEntity(HungarianSoundtrack)), subtitles)
+    val anotherDigitalContainer = DigitalContainer(movie, Set(soundtrack, insertEntity(ItalianSoundtrack)), subtitles)
+    implicit val tx = db.beginTx()
+    transaction(tx) {
+      val digitalContainerNode = subject.createNodeFrom(digitalContainer)
+      val anotherDigitalContainerNode = subject.createNodeFrom(anotherDigitalContainer)
+      digitalContainerNode.getId should not equal(anotherDigitalContainerNode.getId)
+    }
+  }
+
+  test("should create node from digital container if it contains the same movie with subset of soundtracks and same subtitles") {
+    val movie = insertEntity(TestMovie)
+    val subtitles = Set(insertEntity(EnglishSubtitle), insertEntity(HungarianSubtitle))
+    val soundtrack = insertEntity(EnglishSoundtrack)
+    val digitalContainer = DigitalContainer(movie, Set(soundtrack, insertEntity(HungarianSoundtrack)), subtitles)
+    val anotherDigitalContainer = DigitalContainer(movie, Set(soundtrack), subtitles)
+    implicit val tx = db.beginTx()
+    transaction(tx) {
+      val digitalContainerNode = subject.createNodeFrom(digitalContainer)
+      val anotherDigitalContainerNode = subject.createNodeFrom(anotherDigitalContainer)
+      digitalContainerNode.getId should not equal(anotherDigitalContainerNode.getId)
+    }
+  }
+
+  test("should create node from digital container if it contains the same movie with same soundtracks and different subtitles") {
+    val movie = insertEntity(TestMovie)
+    val soundtracks = Set(insertEntity(EnglishSoundtrack), insertEntity(HungarianSoundtrack))
+    val subtitle = insertEntity(EnglishSubtitle)
+    val digitalContainer = DigitalContainer(movie, soundtracks, Set(subtitle, insertEntity(HungarianSubtitle)))
+    val anotherDigitalContainer = DigitalContainer(movie, soundtracks, Set(subtitle, insertEntity(ItalianSubtitle)))
+    implicit val tx = db.beginTx()
+    transaction(tx) {
+      val digitalContainerNode = subject.createNodeFrom(digitalContainer)
+      val anotherDigitalContainerNode = subject.createNodeFrom(anotherDigitalContainer)
+      digitalContainerNode.getId should not equal(anotherDigitalContainerNode.getId)
+    }
+  }
+
+  test("should create node from digital container if it contains the same movie with same soundtracks and subset subtitles") {
+    val movie = insertEntity(TestMovie)
+    val soundtracks = Set(insertEntity(EnglishSoundtrack), insertEntity(HungarianSoundtrack))
+    val subtitle = insertEntity(EnglishSubtitle)
+    val digitalContainer = DigitalContainer(movie, soundtracks, Set(subtitle, insertEntity(HungarianSubtitle)))
+    val anotherDigitalContainer = DigitalContainer(movie, soundtracks, Set(subtitle))
+    implicit val tx = db.beginTx()
+    transaction(tx) {
+      val digitalContainerNode = subject.createNodeFrom(digitalContainer)
+      val anotherDigitalContainerNode = subject.createNodeFrom(anotherDigitalContainer)
+      digitalContainerNode.getId should not equal(anotherDigitalContainerNode.getId)
     }
   }
 }
