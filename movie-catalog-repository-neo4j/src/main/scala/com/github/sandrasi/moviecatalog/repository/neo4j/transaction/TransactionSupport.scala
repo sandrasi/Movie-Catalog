4df537a0be3a1138bdaa26369e5273924f833787
Szabolcs Andrasi <andrasi.szabolcs@gmail.com>
2012-09-15 13:30:28 +0200

    Remove unnecessary empty line

--- movie-catalog-repository-neo4j/src/main/scala/com/github/sandrasi/moviecatalog/repository/neo4j/transaction/TransactionSupport.scala
+++ movie-catalog-repository-neo4j/src/main/scala/com/github/sandrasi/moviecatalog/repository/neo4j/transaction/TransactionSupport.scala
@@ -2,7 +2,6 @@ package com.github.sandrasi.moviecatalog.repository.neo4j.transaction
 
 import org.neo4j.graphdb.{GraphDatabaseService, Transaction}
 
-
 trait TransactionSupport {
 
   protected def transaction[A <: Any](db: GraphDatabaseService)(dbOp: => A): A = transaction(db.beginTx())(dbOp)
