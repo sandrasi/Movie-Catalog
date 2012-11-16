d40fb07c95919d1feff605d4aaf1261ad993e6f1
Szabolcs Andrasi <andrasi.szabolcs@gmail.com>
2012-11-16 22:29:34 +0100

    Create a shorter version of entity DTOs which only contain a minimal information to identify the entity

--- movie-catalog-service/src/main/scala/com/github/sandrasi/moviecatalog/service/dtos/DtoSupport.scala
+++ movie-catalog-service/src/main/scala/com/github/sandrasi/moviecatalog/service/dtos/DtoSupport.scala
@@ -24,51 +24,66 @@ sealed trait MotionPicture extends Entity {
 
   def originalTitle: String
   def localizedTitle: Option[String]
-  def genres: Set[Genre]
-  def runtime: Long
-  def releaseDate: String
+  def genres: Option[Set[Genre]]
+  def runtime: Option[Long]
+  def releaseDate: Option[String]
 }
 
 case class Actor(id: Option[String], person: Person, character: Character, motionPicture: MotionPicture) extends Cast
 
 case class Actress(id: Option[String], person: Person, character: Character, motionPicture: MotionPicture) extends Cast
 
-case class Character(id: Option[String], name: String, creator: String, creationDate: String) extends Entity
+case class Character(id: Option[String], name: String, creator: Option[String] = None, creationDate: Option[String] = None) extends Entity
 
 case class DigitalContainer(id: Option[String], motionPicture: MotionPicture, soundtracks: Set[Soundtrack], subtitles: Set[Subtitle]) extends Entity
 
-case class Genre(id: Option[String], code: String, name: Option[String]) extends Entity
+case class Genre(id: Option[String], code: String, name: Option[String] = None) extends Entity
 
-case class Movie(id: Option[String], originalTitle: String, localizedTitle: Option[String], genres: Set[Genre], runtime: Long, releaseDate: String) extends MotionPicture
+case class Movie(id: Option[String], originalTitle: String, localizedTitle: Option[String] = None, genres: Option[Set[Genre]] = None, runtime: Option[Long] = None, releaseDate: Option[String] = None) extends MotionPicture
 
-case class Person(id: Option[String], name: String, gender: String, dateOfBirth: String, placeOfBirth: String) extends Entity
+case class Person(id: Option[String], name: String, gender: Option[String] = None, dateOfBirth: Option[String] = None, placeOfBirth: Option[String] = None) extends Entity
 
-case class Soundtrack(id: Option[String], languageCode: String, formatCode: String, languageName: Option[String], formatName: Option[String]) extends Entity
+case class Soundtrack(id: Option[String], languageCode: String, formatCode: String, languageName: Option[String] = None, formatName: Option[String] = None) extends Entity
 
-case class Subtitle(id: Option[String], languageCode: String, languageName: Option[String]) extends Entity
+case class Subtitle(id: Option[String], languageCode: String, languageName: Option[String] = None) extends Entity
 
 object DtoSupport {
 
-  private implicit def optionalUuidToOptionalString(id: Option[UUID]) = if (id.isDefined) Some(id.get.toString) else None
+  private implicit def optionalUuidToOptionalString(id: Option[UUID]) = id.map(_.toString)
 
-  implicit def toActorDto(a: domain.Actor)(implicit l: Locale = US): Actor = Actor(a.id, a.person, a.character, a.motionPicture)
+  implicit def toActorDto(a: domain.Actor)(implicit l: Locale = US): Actor = Actor(a.id, toShortPersonDto(a.person), toShortCharacterDto(a.character), toShortMotionPictureDto(a.motionPicture))
 
-  implicit def toActressDto(a: domain.Actress)(implicit l: Locale = US): Actress = Actress(a.id, a.person, a.character, a.motionPicture)
+  implicit def toActressDto(a: domain.Actress)(implicit l: Locale = US): Actress = Actress(a.id, toShortPersonDto(a.person), toShortCharacterDto(a.character), toShortMotionPictureDto(a.motionPicture))
 
-  implicit def toCharacterDto(c: domain.Character): Character = Character(c.id, c.name, c.creator, c.creationDate.toString)
+  implicit def toCharacterDto(c: domain.Character): Character = Character(c.id, c.name, Option(c.creator), Option(c.creationDate.toString))
 
-  implicit def toDigitalContainerDto(dc: domain.DigitalContainer): DigitalContainer = DigitalContainer(dc.id, dc.motionPicture, dc.soundtracks.map(toSoundtrackDto(_)), dc.subtitles.map(toSubtitleDto(_)))
+  private def toShortCharacterDto(c: domain.Character): Character = Character(c.id, c.name)
 
-  implicit def toGenreDto(g: domain.Genre): Genre = Genre(g.id, g.code, if (g.name.isDefined) Some(g.name.get.text) else None)
+  implicit def toDigitalContainerDto(dc: domain.DigitalContainer): DigitalContainer = DigitalContainer(dc.id, toShortMotionPictureDto(dc.motionPicture), dc.soundtracks.map(toShortSoundtrackDto(_)), dc.subtitles.map(toShortSubtitleDto(_)))
+
+  implicit def toGenreDto(g: domain.Genre): Genre = Genre(g.id, g.code, g.name.map(_.text))
+
+  private def toShortGenreDto(g: domain.Genre): Genre = Genre(g.id, g.code)
 
   implicit def toMotionPictureDto(m: domain.MotionPicture)(implicit l: Locale = US): MotionPicture = m match {
-    case movie: domain.Movie => Movie(m.id, m.originalTitle.text, if (m.localizedTitles.exists(_.locale == l)) Some(m.localizedTitles.filter(_.locale == l).head.text) else None, m.genres.map(toGenreDto(_)), m.runtime.getMillis, m.releaseDate.toString)
+    case movie: domain.Movie => Movie(m.id, m.originalTitle.text, if (m.localizedTitles.exists(_.locale == l)) Some(m.localizedTitles.filter(_.locale == l).head.text) else None, Option(m.genres.map(toShortGenreDto(_))), Option(m.runtime.getMillis), Option(m.releaseDate.toString))
     case _ => throw new IllegalArgumentException("Unsupported motion picture type: %s".format(m.getClass.getName))
   }
 
-  implicit def toPersonDto(p: domain.Person): Person = Person(p.id, p.name, p.gender.toString, p.dateOfBirth.toString, p.placeOfBirth)
+  private def toShortMotionPictureDto(m: domain.MotionPicture)(implicit l: Locale = US): MotionPicture = m match {
+    case movie: domain.Movie => Movie(m.id, m.originalTitle.text, if (m.localizedTitles.exists(_.locale == l)) Some(m.localizedTitles.filter(_.locale == l).head.text) else None)
+    case _ => throw new IllegalArgumentException("Unsupported motion picture type: %s".format(m.getClass.getName))
+  }
 
-  implicit def toSoundtrackDto(s: domain.Soundtrack): Soundtrack = Soundtrack(s.id, s.languageCode, s.formatCode, if (s.languageName.isDefined) Some(s.languageName.get.text) else None, if (s.formatName.isDefined) Some(s.formatName.get.text) else None)
+  implicit def toPersonDto(p: domain.Person): Person = Person(p.id, p.name, Option(p.gender.toString), Option(p.dateOfBirth.toString), Option(p.placeOfBirth))
 
-  implicit def toSubtitleDto(s: domain.Subtitle): Subtitle = Subtitle(s.id, s.languageCode, if (s.languageName.isDefined) Some(s.languageName.get.text) else None)
+  private def toShortPersonDto(p: domain.Person): Person = Person(p.id, p.name)
+
+  implicit def toSoundtrackDto(s: domain.Soundtrack): Soundtrack = Soundtrack(s.id, s.languageCode, s.formatCode, s.languageName.map(_.text), s.formatName.map(_.text))
+
+  private def toShortSoundtrackDto(s: domain.Soundtrack): Soundtrack = Soundtrack(s.id, s.languageCode, s.formatCode)
+
+  implicit def toSubtitleDto(s: domain.Subtitle): Subtitle = Subtitle(s.id, s.languageCode, s.languageName.map(_.text))
+
+  private def toShortSubtitleDto(s: domain.Subtitle): Subtitle = Subtitle(s.id, s.languageCode)
 }
--- movie-catalog-service/src/test/scala/com/github/sandrasi/moviecatalog/service/dtos/DtoSupportTest.scala
+++ movie-catalog-service/src/test/scala/com/github/sandrasi/moviecatalog/service/dtos/DtoSupportTest.scala
@@ -19,25 +19,25 @@ class DtoSupportTest extends FunSuite with ShouldMatchers {
 
   test("should convert actor to actor dto") {
     val person = domain.Person("John Joseph Travolta", Male, new LocalDate(1954, 2, 18), "Englewood, New Jersey, U.S.")
-    val character = domain.Character("Vincent Vega")
-    val movie = domain.Movie("Pulp fiction", runtime = Duration.standardMinutes(154), releaseDate = new LocalDate(1994, 10, 14))
+    val character = domain.Character("Vincent Vega", "Quentin Tarantino", new LocalDate(1994, 10, 14))
+    val movie = domain.Movie("Pulp fiction", Set(LocalizedText("Ponyvaregény")(HungarianLocale), LocalizedText("Pulp fiction")(ItalianLocale)), Set(domain.Genre("crime", "Crime"), domain.Genre("thriller", "Thriller")), Duration.standardMinutes(154), new LocalDate(1994, 10, 14))
     val actor = domain.Actor(person, character, movie)
     implicit val locale = HungarianLocale
-    toActorDto(actor) should be(Actor(None, toPersonDto(person), toCharacterDto(character), toMotionPictureDto(movie)))
+    toActorDto(actor) should be(Actor(None, Person(None, "John Joseph Travolta"), Character(None, "Vincent Vega"), Movie(None, "Pulp fiction", Some("Ponyvaregény"))))
   }
 
   test("should convert actress to actress dto") {
     val person = domain.Person("Uma Karuna Thurman", Female, new LocalDate(1970, 4, 29), "Boston, Massachusetts, U.S.")
-    val character = domain.Character("Mia Wallace")
-    val movie = domain.Movie("Pulp fiction", runtime = Duration.standardMinutes(154), releaseDate = new LocalDate(1994, 10, 14))
+    val character = domain.Character("Mia Wallace", "Quentin Tarantino", new LocalDate(1994, 10, 14))
+    val movie = domain.Movie("Pulp fiction", Set(LocalizedText("Ponyvaregény")(HungarianLocale), LocalizedText("Pulp fiction")(ItalianLocale)), Set(domain.Genre("crime", "Crime"), domain.Genre("thriller", "Thriller")), Duration.standardMinutes(154), new LocalDate(1994, 10, 14))
     val actress = domain.Actress(person, character, movie)
     implicit val locale = HungarianLocale
-    toActressDto(actress) should be(Actress(None, toPersonDto(person), toCharacterDto(character), toMotionPictureDto(movie)))
+    toActressDto(actress) should be(Actress(None, Person(None, "Uma Karuna Thurman"), Character(None, "Mia Wallace"), Movie(None, "Pulp fiction", Some("Ponyvaregény"))))
   }
 
   test("should convert charater to character dto") {
     val character = domain.Character("Vincent Vega", "Quentin Tarantino", new LocalDate(1994, 10, 14))
-    toCharacterDto(character) should be(Character(None, character.name, character.creator, character.creationDate.toString))
+    toCharacterDto(character) should be(Character(None, character.name, Some(character.creator), Some(character.creationDate.toString)))
   }
 
   test("should convert digital container to digital container dto") {
@@ -47,27 +47,32 @@ class DtoSupportTest extends FunSuite with ShouldMatchers {
     val englishSubtitle = domain.Subtitle("en", "English")
     val hungarianSubtitle = domain.Subtitle("hu", "Hungarian")
     val digitalContainer = domain.DigitalContainer(movie, Set(englishSoundtrack, hungarianSoundtrack), Set(englishSubtitle, hungarianSubtitle))
-    toDigitalContainerDto(digitalContainer) should be(DigitalContainer(None, toMotionPictureDto(movie), digitalContainer.soundtracks.map(toSoundtrackDto(_)), digitalContainer.subtitles.map(toSubtitleDto(_))))
+    toDigitalContainerDto(digitalContainer) should be(DigitalContainer(None, Movie(None, "Pulp fiction"), Set(Soundtrack(None, "en", "dts"), Soundtrack(None, "hu", "dts")), Set(Subtitle(None, "en"), Subtitle(None, "hu"))))
   }
 
   test("should convert genre to genre dto") {
     val genre = domain.Genre("crime", "Crime")
     toGenreDto(genre) should be(Genre(None, genre.code, Some(genre.name.get.text)))
   }
+
+  test("should convert genre to genre dto if the name is not defined") {
+    val genre = domain.Genre("crime")
+    toGenreDto(genre) should be(Genre(None, genre.code))
+  }
   
   test("should convert movie to movie dto") {
     val movie = domain.Movie("Pulp fiction", Set(LocalizedText("Ponyvaregény")(HungarianLocale), LocalizedText("Pulp fiction")(ItalianLocale)), Set(domain.Genre("crime", "Crime"), domain.Genre("thriller", "Thriller")), Duration.standardMinutes(154), new LocalDate(1994, 10, 14))
-    toMotionPictureDto(movie)(HungarianLocale) should be(Movie(None, movie.originalTitle.text, Some(movie.localizedTitles.filter(_.locale == HungarianLocale).head.text), movie.genres.map(toGenreDto(_)), movie.runtime.getMillis, movie.releaseDate.toString))
+    toMotionPictureDto(movie)(HungarianLocale) should be(Movie(None, movie.originalTitle.text, Some(movie.localizedTitles.filter(_.locale == HungarianLocale).head.text), Some(Set(Genre(None, "crime"), Genre(None, "thriller"))), Some(movie.runtime.getMillis), Some(movie.releaseDate.toString)))
   }
   
   test("should convert movie to movie dto if no localized title matches the current locale") {
     val movie = domain.Movie("Pulp fiction", Set(LocalizedText("Pulp fiction")(ItalianLocale)), Set(domain.Genre("crime", "Crime"), domain.Genre("thriller", "Thriller")), Duration.standardMinutes(154), new LocalDate(1994, 10, 14))
-    toMotionPictureDto(movie)(HungarianLocale) should be(Movie(None, movie.originalTitle.text, None, movie.genres.map(toGenreDto(_)), movie.runtime.getMillis, movie.releaseDate.toString))
+    toMotionPictureDto(movie)(HungarianLocale) should be(Movie(None, movie.originalTitle.text, None, Some(Set(domain.Genre("crime"), domain.Genre("thriller"))), Some(movie.runtime.getMillis), Some(movie.releaseDate.toString)))
   }
   
   test("should convert person to person dto") {
     val person = domain.Person("John Joseph Travolta", Male, new LocalDate(1954, 2, 18), "Englewood, New Jersey, U.S.")
-    toPersonDto(person) should be(Person(None, person.name, person.gender.toString, person.dateOfBirth.toString, person.placeOfBirth))
+    toPersonDto(person) should be(Person(None, person.name, Some(person.gender.toString), Some(person.dateOfBirth.toString), Some(person.placeOfBirth)))
   }
   
   test("should convert soundtrack to soundtrack dto") {
@@ -77,7 +82,7 @@ class DtoSupportTest extends FunSuite with ShouldMatchers {
 
   test("should convert soundtrack to soundtrack dto if the language and format names are not defined") {
     val soundtrack = domain.Soundtrack("en", "dts")
-    toSoundtrackDto(soundtrack) should be(Soundtrack(None, soundtrack.languageCode, soundtrack.formatCode, None, None))
+    toSoundtrackDto(soundtrack) should be(Soundtrack(None, soundtrack.languageCode, soundtrack.formatCode))
   }
 
   test("should convert subtitle to subtitle dto") {
@@ -87,6 +92,6 @@ class DtoSupportTest extends FunSuite with ShouldMatchers {
 
   test("should convert subtitle to subtitle dto if the language and format names are not defined") {
     val subtitle = domain.Subtitle("en")
-    toSubtitleDto(subtitle) should be(Subtitle(None, subtitle.languageCode, None))
+    toSubtitleDto(subtitle) should be(Subtitle(None, subtitle.languageCode))
   }
 }
