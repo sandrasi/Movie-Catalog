caaf3c97108e6e27e8af76980905fb472a99e81c
Szabolcs Andrasi <andrasi.szabolcs@gmail.com>
2014-04-22 21:48:33 -0700

    Reorder test cases

--- movie-catalog-repository-neo4j/src/test/scala/com/github/sandrasi/moviecatalog/repository/neo4j/relationshiptypes/CrewRelationshipTypeTest.scala
+++ movie-catalog-repository-neo4j/src/test/scala/com/github/sandrasi/moviecatalog/repository/neo4j/relationshiptypes/CrewRelationshipTypeTest.scala
@@ -17,23 +17,21 @@ class CrewRelationshipTypeTest extends FunSuite with Matchers {
     CrewRelationshipType.valueOf("Actress") should be(Actress)
   }
 
-  test("should not create crew relationship type value from string not matching any of the enums' name") {
-    intercept[NoSuchElementException] {
-      CrewRelationshipType.valueOf("does not match")
-    }
-  }
-
-  test("should retrieve the name of the crew relationship type") {
-    Actor.name should be(classOf[com.github.sandrasi.moviecatalog.domain.Actor].getName)
-    Actress.name should be(classOf[com.github.sandrasi.moviecatalog.domain.Actress].getName)
-  }
-
   test("should create crew relationship type from bound classes") {
     CrewRelationshipType.forClass(classOf[com.github.sandrasi.moviecatalog.domain.Actor]) should be(Actor)
     CrewRelationshipType.forClass(classOf[com.github.sandrasi.moviecatalog.domain.Actress]) should be(Actress)
   }
 
+  test("should not create crew relationship type value from string not matching any of the enum names") {
+    intercept[NoSuchElementException] { CrewRelationshipType.valueOf("does not match") }
+  }
+
   test("should not create crew relationship type from unsupported an class") {
     intercept[NoSuchElementException] { CrewRelationshipType.forClass(classOf[Any]) }
+  }
+
+  test("should retrieve the name of the crew relationship type") {
+    Actor.name should be(classOf[com.github.sandrasi.moviecatalog.domain.Actor].getName)
+    Actress.name should be(classOf[com.github.sandrasi.moviecatalog.domain.Actress].getName)
   }
 }
